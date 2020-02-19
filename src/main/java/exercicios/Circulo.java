621ed55333bb4399247398267961ee5f3f3137a1
Elker Rodrigues <elker.rodrigues@gmail.com>
2020-02-19 20:53:36 -0300

    Criação da classe Circulo

---
+++ src/main/java/exercicios/Circulo.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Circulo {
+    public Double raio;
+
+    public Double calcularArea(){
+        return 3.14 * (raio*raio);
+    }
+
+    public Double calcularPerimetro(){
+        return 2 * 3.14 * raio;
+    }
+}
